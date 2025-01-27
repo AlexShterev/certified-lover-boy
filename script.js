9c51a09764b939d4619fb22f3f9ff6587d1b429e
AlexShterev <158222766+AlexShterev@users.noreply.github.com>
2025-01-27 20:58:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -34,7 +34,7 @@ function showMessage(response) {
 
     // Update text content, show message, and change image source to "dance.gif"
     const yesMessage = document.getElementById("question");
-    yesMessage.textContent = " Нямам търпение за 14ти, бейби 😘😘";
+    yesMessage.textContent = " Нямам търпение за 14ти, обичам те котенце! 😘😘";
     yesMessage.style.display = "block";
     yesMessage.style.fontStyle = "normal";
     document.getElementsByClassName("image")[0].src = "images/dance.gif";
