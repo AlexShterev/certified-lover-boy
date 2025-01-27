d261d2859ad6de03a1f41c63e6cde849e47320ff
AlexShterev <158222766+AlexShterev@users.noreply.github.com>
2025-01-27 20:52:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function showMessage(response) {
 
     // Update text content and hide name message
     document.getElementById("question").textContent =
-      "Бутона Не, е само за красота";
+      "Бут она Не, е само за красот а";
     document.getElementById("name").style.display = "none";
 
     // Optional: You can also add a timeout to reset the position after a few seconds
